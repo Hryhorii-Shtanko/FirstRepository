f3c9898864eae7798a3db000e5ec80a876a28078
“Hryhorii <gregory.shtanko@yahoo.com>
2022-09-15 20:50:09 +0300

    format some common files

--- collections1/TreeMapEx2.java
+++ collections1/TreeMapEx2.java
@@ -1,10 +1,18 @@
 package collections1;
 
+
 import java.util.TreeMap;
 
-public class TreeMapEx1 {
+public class TreeMapEx2 {
 	public static void main(String[] args) {
 		TreeMap<Double, StudentChDTU> treemap = new TreeMap<>();
+		// TreeMap<StudentChDTU, Double> treemap2= new TreeMap<>(new
+		// Comparator<StudentChDTU>() {
+//			@Override
+//			public int compare(StudentChDTU o1, StudentChDTU o2) {
+//				// TODO Auto-generated method stub
+//				return o1.name.compareTo(o2.name);
+//			}
 
 		StudentChDTU st1 = new StudentChDTU("Zaur", "Tregulov", 3);
 		StudentChDTU st2 = new StudentChDTU("Nikolay", "Mavlof", 28);
@@ -14,7 +22,6 @@ public class TreeMapEx1 {
 		StudentChDTU st7 = new StudentChDTU("Babur", "Bargulov", 2);
 		StudentChDTU st6 = new StudentChDTU("Babur", "Bargulov", 2);
 		StudentChDTU st8 = new StudentChDTU("Pietr", "Sidorov", 2);
-
 		treemap.put(10.2, st1);
 		treemap.put(7.5, st2);
 		treemap.put(6.4, st3);
@@ -26,7 +33,11 @@ public class TreeMapEx1 {
 
 		System.out.println(treemap.containsKey(st7));
 		System.out.println(treemap.containsValue(st6));
+
+		// System.out.println(treemap.get(8.2));
 		System.out.println(treemap.descendingMap());
 		System.out.println(treemap.firstEntry());
+
+		// System.out.println(treemap.containsKey(st3));
 	}
 }
