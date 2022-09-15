7a9be6ebd298e7b9beafa792eb9559c1bfc7355b
--replace-all <gregory.shtanko@yahoo.com>
2022-09-15 22:05:14 +0300

    add some final changes in file test3 and treeMapEx

--- collections1/TreeMapEx2.java
+++ collections1/TreeMapEx2.java
@@ -1,18 +1,17 @@
 package collections1;
 
-
+import java.util.Comparator;
 import java.util.TreeMap;
 
-public class TreeMapEx2 {
+public class TreeMapEx2 implements Comparable<StudentChDTU> {
 	public static void main(String[] args) {
-		TreeMap<Double, StudentChDTU> treemap = new TreeMap<>();
-		// TreeMap<StudentChDTU, Double> treemap2= new TreeMap<>(new
-		// Comparator<StudentChDTU>() {
-//			@Override
-//			public int compare(StudentChDTU o1, StudentChDTU o2) {
-//				// TODO Auto-generated method stub
-//				return o1.name.compareTo(o2.name);
-//			}
+		TreeMap<StudentChDTU, Double> treemap2 = new TreeMap<>(new Comparator<StudentChDTU>() {
+			@Override
+			public int compare(StudentChDTU o1, StudentChDTU o2) {
+				return 0;
+				// o1.name.compareTo(o2.name);
+			}
+		});
 
 		StudentChDTU st1 = new StudentChDTU("Zaur", "Tregulov", 3);
 		StudentChDTU st2 = new StudentChDTU("Nikolay", "Mavlof", 28);
@@ -22,22 +21,27 @@ public class TreeMapEx2 {
 		StudentChDTU st7 = new StudentChDTU("Babur", "Bargulov", 2);
 		StudentChDTU st6 = new StudentChDTU("Babur", "Bargulov", 2);
 		StudentChDTU st8 = new StudentChDTU("Pietr", "Sidorov", 2);
-		treemap.put(10.2, st1);
-		treemap.put(7.5, st2);
-		treemap.put(6.4, st3);
-		treemap.put(8.2, st4);
-		treemap.put(8.3, st5);
-		treemap.put(8.9, st6);
-		treemap.put(8.9, st7);
-		treemap.put(8.1, st8);
+		treemap2.put(st1, 10.2);
+		treemap2.put(st2, 7.5);
+		treemap2.put(st3, 6.4);
+		treemap2.put(st4, 8.2);
+		treemap2.put(st5, 8.3);
+		treemap2.put(st6, 8.9);
+		treemap2.put(st7, 8.9);
+		treemap2.put(st8, 8.1);
 
-		System.out.println(treemap.containsKey(st7));
-		System.out.println(treemap.containsValue(st6));
+		System.out.println(treemap2.containsKey(st7));
+		System.out.println(treemap2.containsValue(st6));
 
-		// System.out.println(treemap.get(8.2));
-		System.out.println(treemap.descendingMap());
-		System.out.println(treemap.firstEntry());
+		System.out.println(treemap2.descendingMap());
+		System.out.println(treemap2.firstEntry());
 
-		// System.out.println(treemap.containsKey(st3));
+		System.out.println(treemap2.containsKey(st3));
+	}
+
+	@Override
+	public int compareTo(StudentChDTU o) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
