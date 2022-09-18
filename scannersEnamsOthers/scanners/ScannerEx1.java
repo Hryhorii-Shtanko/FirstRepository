d5389613f60e5b0e4ac678957f70134e065b9230
--replace-all <gregory.shtanko@yahoo.com>
2022-09-18 08:32:13 +0300

    here there we will do it

---
+++ scannersEnamsOthers/scanners/ScannerEx1.java
@@ -0,0 +1,15 @@
+package scanners;
+
+import java.util.Scanner;
+
+public class ScannerEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scanner = new Scanner (System.in);
+System.out.println("Input number");
+int i = scanner.nextInt();
+System.out.println("Inputed number" + i);
+	}
+
+}
