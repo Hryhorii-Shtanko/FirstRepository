02933ad59d43939b81e9a6d4efc45bb828ce394e
Hryhorii-Shtanko <112967804+Hryhorii-Shtanko@users.noreply.github.com>
2022-09-18 22:15:51 +0300

    add third file

---
+++ scannersEnamsOthers/scanners/ScannerEx3.java
@@ -0,0 +1,16 @@
+package scanners;
+
+import java.util.Scanner;
+
+public class ScannerEx3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner (System.in);
+
+		System.out.println("Write few words");
+		String s = scanner.nextLine();
+		System.out.println("You wrote: " + s);
+		}
+
+}
