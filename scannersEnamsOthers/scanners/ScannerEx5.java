51019a56f1d6054f9da893b307dc53125ff899d6
Hryhorii-Shtanko <112967804+Hryhorii-Shtanko@users.noreply.github.com>
2022-09-19 23:10:34 +0300

    add 5 file

---
+++ scannersEnamsOthers/scanners/ScannerEx5.java
@@ -0,0 +1,27 @@
+package scanners;
+
+import java.util.Scanner;
+
+public class ScannerEx5 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner (System.in);
+
+		System.out.println("Write the Line");
+		String s1 = scanner.next();
+		System.out.println("You wrote : " + s1);
+		
+	}
+
+}
+class Scanner5 {
+public static void main(String[] args) {
+	
+Scanner scanner = new Scanner (System.in);
+System.out.println("Input fractional number");
+double d = scanner.nextDouble();
+System.out.println("Inputed number" + d);
+}
+}
+
+
