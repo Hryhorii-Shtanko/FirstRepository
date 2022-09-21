5cae490827729401691319d70490760c78adb176
Hryhorii-Shtanko <112967804+Hryhorii-Shtanko@users.noreply.github.com>
2022-09-21 10:37:09 +0300

    add 6 part of scanners

---
+++ scannersEnamsOthers/scanners/ScannerEx6.java
@@ -0,0 +1,19 @@
+package scanners;
+
+import java.util.Scanner;
+
+public class ScannerEx6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner ("Hi my Friend!"+"\nHow you doing?"+"\nWhat is new");
+String s1 = scanner.next();
+System.out.println(s1);
+System.out.println(scanner.nextLine());
+System.out.println(scanner.nextLine());
+
+		
+	}
+
+}
+ 
