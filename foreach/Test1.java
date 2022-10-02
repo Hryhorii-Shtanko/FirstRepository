f808342dd8a20f6a66de2fc45fc741015d0c9319
Hryhorii-Shtanko <112967804+Hryhorii-Shtanko@users.noreply.github.com>
2022-10-02 19:24:00 +0300

    Add Foreach loop first file

---
+++ foreach/Test1.java
@@ -0,0 +1,11 @@
+package foreach;
+
+public class Test1 {
+public static void main(String[] args) {
+	int[]array = {0,6,4,1};
+	for(int i=0;i< array.length;i++) {
+		System.out.print(array[i]+" ");
+	}
+	System.out.println();
+}
+}
